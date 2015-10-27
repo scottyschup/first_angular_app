cf122b77c20ea7b5d32d0a606826fe0a2cab1532
Scott Schupbach <sschupbach@asperasoft.com>
2015-10-26 19:43:12 -0700

    Add getNames, post to names, and deleteName functionality

--- js/modules/api_monkey/api_monkey.js
+++ js/modules/api_monkey/api_monkey.js
@@ -7,20 +7,45 @@ function apiMonkey ($http) {
 
   apiFlyingMonkey.API_SERVER = 'http://127.0.0.1:8080';
   apiFlyingMonkey.API_VERSION = '/v1';
-  apiFlyingMonkey.get = get;
+  apiFlyingMonkey.delete = deleteReq;
+  apiFlyingMonkey.get = getReq;
   apiFlyingMonkey.ping = ping;
+  apiFlyingMonkey.post = postReq;
+  apiFlyingMonkey.simulateDelay = simulateDelay;
+  apiFlyingMonkey.update = updateReq;
 
-  function get (path) {
+  function simulateDelay (cb, ms) {
+    ms = ms || 500;
+    setTimeout(function () {
+      cb();
+    }, ms);
+  }
+
+  function deleteReq (path, id) {
+    var url = this.API_SERVER + this.API_VERSION + path + "/" + id;
+    return $http.delete(url);
+  }
+
+  function getReq (path) {
     var url = this.API_SERVER + this.API_VERSION + path;
     return $http.get(url);
+  }
+
+  function postReq (path, data) {
+    var url = this.API_SERVER + this.API_VERSION + path;
+    return $http.post(url, data);
   }
 
   function ping () {
     return $http.get(this.API_SERVER + '/hello')
       .then(function (res) {
-        console.log(res);
         return res;
       });
+  }
+
+  function updateReq (path, data) {
+    var url = this.API_SERVER + this.API_VERSION + path + "/" + data.id;
+    return $http.update(url, data);
   }
 
   return apiFlyingMonkey;
--- js/modules/names/namesCtrl.js
+++ js/modules/names/namesCtrl.js
@@ -2,26 +2,57 @@ angular
   .module('firstApp')
   .controller('NamesCtrl', namesCtrl);
 
-function namesCtrl (formatText) {
+function namesCtrl (formatText, apiMonkey, $scope) {
   var self = this;
 
   self.addName = addName;
+  self.allNames = [];
   self.currentName = '';
+  self.deleteName = deleteName;
+  self.getAllNames = getAllNames;
   self.greet = greet;
   self.hoverInput = hoverInput;
   self.inputDisabled = false;
+  self.message = '';
   self.removeInput = removeInput;
+  self.removeNameFromList = removeNameFromList;
   self.revealInput = revealInput;
+
+  self.getAllNames();
 
   function addName (newName) {
     self.names = {};
+
     self.names.titleName = { case: 'Title Case', name: formatText.titleCase(newName) };
     self.names.camelName = { case: 'Camel Case', name: formatText.camelCase(newName) };
     self.names.snakeName = { case: 'Snake Case', name: formatText.snakeCase(newName) };
     self.names.upperName = { case: 'Upper Case', name: formatText.upperCase(newName) };
     self.names.lowerName = { case: 'Lower Case', name: formatText.lowerCase(newName) };
 
-    self.removeInput();
+    var data = { name: { "name": newName } };
+    apiMonkey.post('/names', data)
+      .then(function (res) {
+        self.allNames.push(res.data);
+        self.removeInput();
+      });
+  }
+
+  function deleteName (id) {
+    var cb = function () {
+      apiMonkey.delete("/names", id)
+        .then(function (res) {
+          self.removeNameFromList(id);
+        });
+    }
+
+    apiMonkey.simulateDelay(cb);
+  }
+
+  function getAllNames () {
+    apiMonkey.get("/names")
+      .then(function (res) {
+        self.allNames = res.data;
+      });
   }
 
   function greet (currentName) {
@@ -31,8 +62,17 @@ function namesCtrl (formatText) {
   function hoverInput (ev) {
     var x = ev.x,
         y = ev.y;
+    self.$tooltip = $(document.querySelector('#tooltip'));
+    self.$tooltip.css({
+      'left': x + 'px',
+      'top': y + 'px',
+      'opacity': '1'
+    });
 
-    console.log(x, y);
+    setTimeout(function () {
+      self.$tooltip.css('opacity', '0');
+    }, 1500);
+
   }
 
   function removeInput () {
@@ -41,7 +81,14 @@ function namesCtrl (formatText) {
     }
   }
 
+  function removeNameFromList (id) {
+    self.allNames = self.allNames.filter(function (name) {
+      return name.id !== id;
+    })
+  }
+
   function revealInput () {
+    self.currentName = '';
     self.inputDisabled = false;
   }
 }
